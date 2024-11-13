4edb54f84fa20a51beb1ce223c59fd97ab77ffc2
Vladimir Buravlev <Vladimir.Buravlev@virtoway.com>
2024-11-13 09:03:54 +0100

    VM-1498: communications for vc-shell front

--- src/VirtoCommerce.MarketplaceCommunicationModule.Web/vcmp-communication/src/modules/messenger/pages/overrides/index.ts
+++ src/VirtoCommerce.MarketplaceCommunicationModule.Web/vcmp-communication/src/modules/messenger/pages/overrides/index.ts
@@ -8,5 +8,17 @@ export const overrides: OverridesSchema = {
       index: 4,
       value: "MessageWidget",
     },
+    {
+      id: "OrderDetails",
+      path: "content.orderWidgets.children",
+      index: 1,
+      value: "MessageWidget",
+    },
+    {
+      id: "Offer",
+      path: "content.offerWidgets.children",
+      index: 1,
+      value: "MessageWidget",
+    },
   ],
 };
