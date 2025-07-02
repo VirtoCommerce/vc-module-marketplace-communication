2f384b359d93a685c506cb9476d21cd30185ab21
Maksim <maxim.zinchuk@gmail.com>
2025-07-02 15:03:26 +0300

    refactor: clean up type declarations in shims-vue.d.ts by removing unused imports and simplifying module definitions

--- src/VirtoCommerce.MarketplaceCommunicationModule.Web/vcmp-communication/src/shims-vue.d.ts
+++ src/VirtoCommerce.MarketplaceCommunicationModule.Web/vcmp-communication/src/shims-vue.d.ts
@@ -1,18 +1,8 @@
 /* eslint-disable */
-import { CoreBladeAdditionalSettings, DynamicGridSchema, DynamicDetailsSchema } from "@vc-shell/framework";
 
-import type { Component, Ref } from "vue";
-import type {
-  ComponentCustomProperties as _ComponentCustomProperties,
-} from 'vue';
+import { CoreBladeAdditionalSettings } from "@vc-shell/framework";
+import type { Ref } from "vue";
 import type { Composer } from "vue-i18n";
-import type { moment } from "moment";
-import type { Vue } from "vue";
-import type { VueRouter } from "vue-router";
-import type { VeeValidate } from "vee-validate";
-import type { VueI18n } from "vue-i18n";
-import type { VcShellFramework } from "@vc-shell/framework";
-
 
 declare module "*.vue" {
   import type { DefineComponent } from "vue";
@@ -30,40 +20,9 @@ declare module "@vue/runtime-core" {
     $isDesktop: Ref<boolean>;
     $isTouch: boolean;
     $t: (key: string, ...args: any[]) => string;
-    $dynamicModules: {
-      [x: string]: {
-        components?: { [key: string]: Component };
-        composables?: { [key: string]: (...args: any[]) => any };
-        default: { install: (app: any, options?: any) => void };
-        schema: { [key: string]: DynamicGridSchema | DynamicDetailsSchema };
-        locales: { [key: string]: { [key: string]: string } };
-        notificationTemplates?: { [key: string]: Component };
-      };
-    };
   }
 
   interface ComponentOptionsBase extends CoreBladeAdditionalSettings {}
-}
-
-declare global {
-  interface Window {
-    VcShellDynamicModules: {
-      [x: string]: {
-        components?: { [key: string]: Component };
-        composables?: { [key: string]: (...args: any[]) => any };
-        default: { install: (app: any, options?: any) => void };
-        schema: { [key: string]: DynamicGridSchema | DynamicDetailsSchema };
-        locales: { [key: string]: { [key: string]: string } };
-        notificationTemplates?: { [key: string]: Component };
-      };
-    };
-    Vue: typeof vue;
-    VueRouter: typeof vueRouter;
-    VeeValidate: typeof veeValidate;
-    VueI18n: typeof vueI18n;
-    moment: moment;
-    VcShellFramework: any;
-  }
 }
 
 export {};
